ade260410f21b87a96664c327d81019a1fc5d081
Smithry9 <smithry9@oregonstate.edu>
2021-05-02 23:31:30 -0700

    added basic error checking to fullName code

---
+++ fullName.py
@@ -0,0 +1,13 @@
+def fullname(firstName, lastName):
+    if(isValid(firstName) and isValid(lastName)):
+        return firstName + " " + lastName
+    else:
+        return "Not a valid input"
+
+
+
+def isValid(inputString):
+    if(type(inputString) is str):
+        return True
+    else:
+        return False
