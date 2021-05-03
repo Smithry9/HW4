d4a797b37337ffac87040a0b71cf477c98e34d53
Smithry9 <smithry9@oregonstate.edu>
2021-05-02 22:56:40 -0700

    Added error handling and test cases

--- cubeVolume.py
+++ cubeVolume.py
@@ -3,5 +3,17 @@
 #smithry9@oregonstate.edu
 
 def volume(length, width, height):
-    vol = length * width * height
-    return vol
+    if(isValid(length) and isValid(width) and isValid(height)):
+        vol = length * width * height
+        return vol
+    else:
+        return "Not a valid input"
+
+def isValid(value):
+    if((type(value) is int) or (type(value) is float)):
+        if(value >= 0):
+            return True
+        else:
+            return False
+    else:
+        return False
--- test_cubeVolume.py
+++ test_cubeVolume.py
@@ -3,8 +3,29 @@ import cubeVolume
 
 class testCaseAdd(unittest.TestCase):
     #test cases
+    #cube with lenght, width, and height = 2
     def test_volume1(self):
         self.assertEqual(cubeVolume.volume(2,2,2), 8)
+
+    #cube with length = 1.5, width = 2, height = 2.5
+    def test_volume2(self):
+        self.assertEqual(cubeVolume.volume(1.5,2,2.5), 7.5)
+
+    #cube with length = 0 (makes the volume 0)
+    def test_volume3(self):
+        self.assertEqual(cubeVolume.volume(0,2,2), 0)
+
+    #invalid result with negative number
+    def test_volume4(self):
+        self.assertEqual(cubeVolume.volume(2,-1,2), "Not a valid input")
+
+    #invalid result with character
+    def test_volume5(self):
+        self.assertEqual(cubeVolume.volume(2,'f',2), "Not a valid input")
+
+    #invalid result with string
+    def test_volume6(self):
+        self.assertEqual(cubeVolume.volume("hello",2,2), "Not a valid input")
 
 
 if __name__ == "__main__":
